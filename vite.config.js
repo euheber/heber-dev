300f6ad16cd51e56975f0690dfabd58047bd18a7
euheber <euheber1@gmail.com>
2023-01-16 20:20:10 -0300

    removed base for testing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/heber-dev/',
+  // base: '/heber-dev/',
   plugins: [vue()],
   resolve: {
     alias: {
